6f85056347a768cb560c6e185189d9d4a9554572
kjm <aaggf1112@gmail.com>
2020-03-17 18:11:55 +0900

    작업중

--- background.js
+++ background.js
@@ -1,3 +1,26 @@
+var disabledCheck = false;
+
+var onHeadersReceived = function(headers) {
+	if(!disabledCheck){
+		return;
+	}
+	for (var i = 0; i < headers.responseHeaders.length; i++) {
+		if(details.responseHeaders[i].name.toUpperCase() === "CONTENT-SECURITY-POLICY") {
+			headers.responseHeaders[i].value = "";
+		}
+	}
+	return {
+		responseHeaders: headers.responseHeaders
+	};
+});
+
+var filter = {urls: ["*://*/*"], types: ["main_frame", "sub_frame"]};
+
+chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, filter, ["blocking", "responseHeaders"]);
+  
 chrome.browserAction.onClicked.addListener(
-    
+	disabledCheck = !disabledCheck;
+	if(disabledCheck){
+	
+	}
 });
