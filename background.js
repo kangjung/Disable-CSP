515acc81f273b579ade2c2f79fdebb15988a42da
kjm <aaggf1112@gmail.com>
2020-03-19 09:42:48 +0900

    아이콘 추가

--- background.js
+++ background.js
@@ -1,26 +1,37 @@
 var disabledCheck = false;
 
-var onHeadersReceived = function(headers) {
+var onHeadersReceived = function(details) {
 	if(!disabledCheck){
 		return;
 	}
-	for (var i = 0; i < headers.responseHeaders.length; i++) {
+	for (var i = 0; i < details.responseHeaders.length; i++) {
 		if(details.responseHeaders[i].name.toUpperCase() === "CONTENT-SECURITY-POLICY") {
-			headers.responseHeaders[i].value = "";
+			details.responseHeaders[i].value = "";
 		}
 	}
 	return {
-		responseHeaders: headers.responseHeaders
+		responseHeaders: details.responseHeaders
 	};
-});
+};
 
 var filter = {urls: ["*://*/*"], types: ["main_frame", "sub_frame"]};
 
+
+var imageChange = function() {
+	var image = disabledCheck ? "image/icon.png" : "image/iconX.png"
+	var title = disabledCheck ? "disabled" : "enabled";
+
+	chrome.browserAction.setIcon({ path: image });
+	chrome.browserAction.setTitle({ title: 'CSP ' + title });
+};
+
+
 chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, filter, ["blocking", "responseHeaders"]);
-  
-chrome.browserAction.onClicked.addListener(
+
+chrome.browserAction.onClicked.addListener(function() {
 	disabledCheck = !disabledCheck;
 	if(disabledCheck){
-	
+		chrome.browsingData.remove({}, {"serviceWorkers": true}, function () {});
 	}
+	imageChange()
 });
