313c5d31ae4646b7e0f94606d6309c2cdd9aa2f7
kjm <aaggf1112@gmail.com>
2020-03-30 16:23:46 +0900

    코드 정리

--- background.js
+++ background.js
@@ -1,37 +1,39 @@
 var disabledCheck = false;
 
-var onHeadersReceived = function(details) {
-	if(!disabledCheck){
-		return;
-	}
-	for (var i = 0; i < details.responseHeaders.length; i++) {
-		if(details.responseHeaders[i].name.toUpperCase() === "CONTENT-SECURITY-POLICY") {
-			details.responseHeaders[i].value = "";
-		}
-	}
-	return {
-		responseHeaders: details.responseHeaders
-	};
-};
+function imageChange() {
+    var image = disabledCheck ? "image/icon.png" : "image/iconX.png";
+    var title = disabledCheck ? "disabled" : "enabled";
 
+    chrome.browserAction.setIcon({ path: image });
+    chrome.browserAction.setTitle({ title: 'CSP ' + title });
+}
+
+
+var opt_extraInfoSpec = ["blocking", "responseHeaders"];
 var filter = {urls: ["*://*/*"], types: ["main_frame", "sub_frame"]};
+var callback = function(details) {
+    if(!disabledCheck){
+        return;
+    }
+    for (var i = 0; i < details.responseHeaders.length; i++) {
+        if(details.responseHeaders[i].name.toUpperCase() === "CONTENT-SECURITY-POLICY") {
+            details.responseHeaders[i].value = "";
+        }
+    }
 
-
-var imageChange = function() {
-	var image = disabledCheck ? "image/icon.png" : "image/iconX.png"
-	var title = disabledCheck ? "disabled" : "enabled";
-
-	chrome.browserAction.setIcon({ path: image });
-	chrome.browserAction.setTitle({ title: 'CSP ' + title });
+    imageChange();
+    return {
+        responseHeaders: details.responseHeaders
+    };
 };
 
 
-chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, filter, ["blocking", "responseHeaders"]);
+chrome.webRequest.onHeadersReceived.addListener(callback, filter, opt_extraInfoSpec);
 
 chrome.browserAction.onClicked.addListener(function() {
 	disabledCheck = !disabledCheck;
 	if(disabledCheck){
 		chrome.browsingData.remove({}, {"serviceWorkers": true}, function () {});
 	}
-	imageChange()
+	imageChange();
 });
